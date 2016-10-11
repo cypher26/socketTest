2da9bbf95b5a2c8cea12dfa48a403c76ae21b9ca
cypher26 <jestercaporado@yahoo.com>
2016-10-11 16:39:42 +0800

    1

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+
+io.sockets.on('connection', function (socket) {
+		 console.log('Client connected hehe');
+
+
+});
+
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+
+
+
+// });
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
