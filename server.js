e5b82bf7d6cda74a3067105ea481fc7dac9f22f8
cypher26 <jestercaporado@yahoo.com>
2016-10-11 19:08:30 +0800

    2

--- server.js
+++ server.js
@@ -4,12 +4,29 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 
+
+
 const PORT = process.env.PORT || 5000;
-const INDEX = path.join(__dirname, 'index.html');
+// const INDEX = path.join(__dirname, '/views/index.html');
+
+var router = express.Router();
+router.get("/",function(req,res){
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
+	.use("/",router)
+	.use("",express.static(__dirname + '/views'))
+	.use("js",express.static(__dirname + '/js'))
+	.use("/node_modules/angular/",express.static(__dirname + '/node_modules/angular/'))
+  // .use(function(req, res) {
+  // 		res.sendFile(INDEX); 
+  // 		// res.sendFile(INDEX); 
+
+  // })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
 
 const io = socketIO(server);
 
@@ -24,8 +41,10 @@ io.sockets.on('connection', function (socket) {
 //   console.log('Client connected');
 //   socket.on('disconnect', () => console.log('Client disconnected'));
 
-
+	
 
 // });
+
+
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
